806b709c15facd0e1d0eb029116583e2d3e8a710
munozyanez <munoz.yanez@gmail.com>
2017-05-09 21:19:46 +0200

    modified:   CMakeLists.txt 	modified:   IKinematics.cpp 	modified:   IKinematics.hpp 	new file:   ikfast/CMakeLists.txt 	new file:   ikfast/ikfastRightLeg.cpp 	new file:   ikfast/ikfastRightLeg.h 	new file:   ikfast/leftLeg/CMakeLists.txt 	new file:   ikfast/leftLeg/ikfastLeftLeg.cpp 	new file:   ikfast/leftLeg/ikfastLeftLeg.h

--- IKinematics.hpp
+++ IKinematics.hpp
@@ -1,47 +1,22 @@
-#ifndef PLOT_H
-#define PLOT_H
+#ifndef IKINEMATICS_H
+#define IKINEMATICS_H
 
 
 #include <vector>
-#include <plotter.h>
-#include <plot.h>
 #include <fstream>      // std::fstream
 #include <algorithm>    // std::min_element, std::max_element
 
 #include <iostream>
-//#include <plplot/plplot.h>
-//#include <plplot/plstream.h>
-#define NSIZE    101
+//#include "ikfastLeftLeg.h"
 
-class IPlot
+class IKinematics
 {
 public:
-    IPlot();
-    IPlot(double sampleTime);
-
-    long pushBack(double new_value);
-
-    long Plot();
-    long Save(string filename);
-
-    long Plot(std::vector<double> datax, std::vector<double> datay, double scalex, double scaley);
-    long PlotAndSave(std::vector<double> datax, std::vector<double> datay, double scalex, double scaley, std::string filename);
-
+    IKinematics();
 
 private:
 
 
-
-    double Ts;
-
-    std::vector<double> x,y;
-
-    XPlotter plt;
-
-
-    long InitPlot();
-
-    //PlotterParams puPlotParams;
 };
 
-#endif // PLOT_H
+#endif // IKINEMATICS_H
